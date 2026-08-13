a90a3ce930d5715cd1513385a8763a644e176da6
kyle <ktdavison29@gmail.com>
2026-08-13 12:59:39 -0400

    Cleaning up the game over a little bit

--- src/scenes/GameOver.tsx
+++ src/scenes/GameOver.tsx
@@ -1,5 +1,6 @@
 import { GamePhase } from '../types/game.types';
 import { useEndGameState } from '../engine/game.engine.context.ts';
+import './game.over.css'
 
 interface SceneProps {
   onTransition: (nextPhase: GamePhase) => void;
@@ -20,14 +21,14 @@ export function GameOver({ onTransition }: SceneProps) {
     victoryDeclarationText = `${winningPlayers[0].name} wins the match!`;
   } else if (winningPlayers.length > 1) {
     const jointNamesText = winningPlayers.map((p) => p.name).join(' & ');
-    victoryDeclarationText = `Joint Victory! Congratulations to ${jointNamesText}!`;
+    victoryDeclarationText = `${jointNamesText} win the match!`;
   }
 
   return (
     <div className="game-over-screen">
       <h2>Match Over!</h2>
-      <div>winner is {victoryDeclarationText}</div>
-      <button onClick={() => onTransition(GamePhase.MainMenu)}>
+      <div className='winner-text'>{victoryDeclarationText}</div>
+      <button className='main-menu-button' onClick={() => onTransition(GamePhase.MainMenu)}>
         Return to Title Screen
       </button>
     </div>
